381f30690c8df537fa5fb89ce5b112fb7aab34a2
DonMashiro <dawid.masiarek@adres.pl>
2024-09-04 19:19:50 +0200

    Gotowa funkcja, która sprawdza, czy dany wyraz jest palindromem

--- palindromy.py
+++ palindromy.py
@@ -1,1 +1,13 @@
-
+def palindrome(word):
+  """Funkcja służy do sprawdzenia, czy dany wyraz jest palindromem. Palindrom słowo, które czytane od lewej do prawej i od prawej do lewej brzmi tak samo. Przykłady to “kajak” i “potop”.
+  Argument:
+  word: sprawdzany wyraz.
+  Returns:
+  True, jeśli tekst jest palindromem, False jeśli nie.
+  """
+  word = word.lower() 
+  return word == word[::-1]  
+print(palindrome("koza"))
+print(palindrome("sedes"))
+print(palindrome("kajak"))
+print(palindrome("bąk"))
